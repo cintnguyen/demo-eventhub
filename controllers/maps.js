306a3cb48ce2e9579c609afc143595b86971125f
cintnguyen <cintnguyen98@gmail.com>
2023-05-24 16:34:01 -0400

    updated CSS

--- controllers/maps.js
+++ controllers/maps.js
@@ -4,7 +4,7 @@ module.exports = {
   getMaps: async (req, res) => {
     try {
       const event = await Event.findById(req.params.id);
-      res.render("maps.ejs", { eventID: event._id, address: event.address, user: req.user});
+      res.render("maps.ejs", { eventID: event._id, address: event.address, user: req.user, eventName: event.name,});
     } catch (err) {
       console.log(err);
     }
