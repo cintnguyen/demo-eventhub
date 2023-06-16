45e1502a984110ea1eeaf2c96073fe9c52020e46
cintnguyen <cintnguyen98@gmail.com>
2023-06-15 21:40:25 -0400

    removed extra file

--- controllers/events.js
+++ controllers/events.js
@@ -1,10 +1,7 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
 const Comment = require("../models/Comment")
-// const Contact = require("../models/Contact")
 const Event = require("../models/Event")
-// const Task = require("../models/Task")
-
 
 module.exports = {
   getDashboard: async (req, res) => {
@@ -15,19 +12,6 @@ module.exports = {
       console.log(err);
     }
   },
-  // createComment: async (req, res) => {
-  //   try {
-  //     await Comment.create({
-  //       comment: req.body.comment,
-  //       user: req.user.id,
-  //       postId : req.params.id
-  //     });
-  //     console.log("Comment has been added!");
-  //     res.redirect(`/post/${req.params.id}`);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // },
   createEvent: async (req, res) => {
     try {
       const result = await cloudinary.uploader.upload(req.file.path);
@@ -66,10 +50,6 @@ module.exports = {
     try {
       const event = await Event.findById(req.params.id);
       event.deleteOne({_id: event.id})
-      // const guestIndex = Number(req.body.guest)
-      // const invitedGuest = event.guests[guestIndex]
-      //event.markModified('guests'); // method for mongoose to recognize that we made changes to the guest array 
-      // await event.save()
       res.redirect(`/dashboard#cnevents`);
     } catch (err) {
       console.log(err);
